bcb9d09eb7b3f842dbe95cdd1c27a37997d99d13
Lee Brown <lee@fossa.com>
2021-12-13 14:42:11 -0500

    Add part 2 code.

--- day1/part1.lua
+++ day1/part1.lua
@@ -8,12 +8,24 @@ end
 
 function find_changes () 
     values = read_file()
+    local windows = {}
     local increaseCount = 0
+
     for i,v in pairs(values)
     do
-        if i ~= 1 then
-            if values[i] > values[i-1] then
-                increaseCount = increaseCount + 1
+        if i == 1 then
+            windows[i] = v
+        elseif i == 2 then
+            windows[i-1] = windows[i-1] + v
+            windows[i] = v
+        else
+            windows[i-2] = windows[i-2] + v
+            windows[i-1] = windows[i-1] + v
+            windows[i] = v
+            if i >= 4 then
+                if windows[i-2] > windows[i-3] then
+                    increaseCount = increaseCount + 1
+                end
             end
         end
     end
